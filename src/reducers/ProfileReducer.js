8e59dbb4c66c0ca343a66f16d491dcb14584ffea
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-03-03 23:31:15 +0600

    Profile context, provider,reducer and actions created

---
+++ src/reducers/ProfileReducer.js
@@ -0,0 +1,42 @@
+import { actions } from "../actions";
+
+const initialState = {
+  user: null,
+  posts: [],
+  loading: false,
+  error: null,
+};
+
+const ProfileReducer = (state, action) => {
+  switch (action.type) {
+    case actions.profile.DATA_FETCHING: {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+
+    case actions.profile.DATA_FETCHED: {
+      return {
+        ...state,
+        loading: false,
+        user: action.data.user,
+        posts: action.data.posts,
+      };
+    }
+
+    case actions.profile.DATA_FETCH_ERROR: {
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
+
+export { ProfileReducer, initialState };
