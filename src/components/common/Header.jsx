4a956284a1a70aa3bc4619c8fc1b06e8158ee15c
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-03-11 17:29:27 +0600

    Updated logo

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -9,7 +9,7 @@ import { useAuth } from "../../hooks/useAuth";
 import { useProfile } from "../../hooks/useProfile";
 
 const Header = () => {
-  const {state} = useProfile();
+  const { state } = useProfile();
   const { auth } = useAuth();
 
   const user = state?.user ?? auth?.user;
@@ -18,10 +18,9 @@ const Header = () => {
     <nav className="sticky top-0 z-50 border-b border-[#3F3F3F] bg-[#1E1F24] py-4">
       <div className="container flex flex-col items-center justify-between gap-6 sm:flex-row">
         <Link to="/">
-          <img
-            className="max-w-[100px] rounded-full lg:max-w-[130px]"
-            src={Logo}
-          />
+          <h1 className="my-2 text-3xl font-bold lg:text-[40px]">
+            Link <span className="text-lg text-[#00D991] -ml-[8px]">Loom</span>
+          </h1>
         </Link>
 
         <div className="flex items-center space-x-4">
@@ -37,7 +36,7 @@ const Header = () => {
 
           <Link to="/me" className="flex-center !ml-8 gap-3">
             <span className="text-lg font-medium lg:text-xl">
-              {user?.firstName} {' '}{user?.lastName}
+              {user?.firstName} {user?.lastName}
             </span>
             <img
               className="max-h-[32px] max-w-[32px] lg:max-h-[44px] lg:max-w-[44px] rounded-full"
